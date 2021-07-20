15b504ffbdf196452da5e9a00b4133968eccedd0
Davidosky007 <Davidosky1@gmail.com>
2021-07-20 14:56:05 +0100

    Add all actions creators and actions

---
+++ src/actions/index.js
@@ -0,0 +1,40 @@
+const ADD_VALUE = 'ADD_VALUE';
+const CHANGE_DATE = 'CHANGE_DATE';
+const CHANGE_FILTER = 'CHANGE_FILTER';
+const GET_UNITS = 'GET_UNITS';
+const GET_MEASUREMENTS = 'GET_MEASUREMENTS';
+
+const actions = {
+  GET_MEASUREMENTS, GET_UNITS, ADD_VALUE, CHANGE_DATE, CHANGE_FILTER,
+};
+
+const getUnits = (units) => ({
+  type: GET_UNITS,
+  units,
+});
+
+const getAllMeasurements = (measurements) => ({
+  type: GET_MEASUREMENTS,
+  measurements,
+});
+
+const addValue = (unit, value) => ({
+  type: ADD_VALUE,
+  unit,
+  value,
+});
+
+const changeDate = (date) => ({
+  type: CHANGE_DATE,
+  date,
+});
+
+const changeFilter = (filter) => ({
+  type: CHANGE_FILTER,
+  filter,
+});
+
+export default actions;
+export {
+  getUnits, getAllMeasurements, addValue, changeDate, changeFilter,
+};
