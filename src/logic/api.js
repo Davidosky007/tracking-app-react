dfdc67032ebd2a1bd2591b84e61e3a637ee749ec
Davidosky007 <Davidosky1@gmail.com>
2021-07-27 17:16:40 +0100

    Push some update to fix another 401 bug

--- src/logic/api.js
+++ src/logic/api.js
@@ -31,7 +31,6 @@ const saveMeasurement = async (id, value, token) => {
 const fetchAllMeasurements = async (token) => {
   const response = await fetch(`${base}/measurements`, {
     mode: 'cors',
-    method: 'GET',
     headers: {
       Authorization: `Bearer ${token}`,
     },
